1e2e42f090bcd440a1cb025c829ba64783589b58
TwanMulder <twan.mulder@gmail.com>
2021-01-03 23:02:50 +0100

    bunch of setup stuff

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font text-4xl sm:text-6xl mb-4 font-bold text-gray-900">GitHub Templates</h1>
+          <h2 className="mb-8 text-lg sm:text-xl leading-relaxed">An overview of insightful README's to get you started in seconds.</h2>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["PT Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
