35a4b73315ff00ee8e3dae81a2387dd9057e5aaa
TwanMulder <twan.mulder@gmail.com>
2021-01-05 12:28:37 +0100

    remove unused cra files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import "typeface-pt-sans";
 import "github-markdown-css";
 
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,8 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
